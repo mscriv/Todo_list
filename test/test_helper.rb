2ab106c956668c3bf3e808903c82a0f1c4731c8a
Michael Scriven <bigscriv03@gmail.com>
2014-05-01 00:19:17 +0000

    Add rspec and capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'rspec/rails'
+require 'rspec/autorum'
+require 'caybara/rspec'
+
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
