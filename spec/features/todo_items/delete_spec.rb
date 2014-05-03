4203d73e1e40a8a557ec91e56db020c50ab6fc0a
Michael Scriven <bigscriv03@gmail.com>
2014-05-03 15:41:22 +0000

    Extract todo list helper

--- spec/features/todo_items/delete_spec.rb
+++ spec/features/todo_items/delete_spec.rb
@@ -2,15 +2,8 @@ require 'spec_helper'
 
 describe "Deleting todo items" do
   let!(:todo_list)  {TodoList.create(title: "Grocery list", description: "Groceries")}
-  let!(:todo_item)  {todo_list.todo_items.create(content: "Milk") }
-  
-  def visit_todo_list(list)
-    visit "/todo_lists"
-    within "#todo_list_#{list.id}" do
-      click_link "List Items"
-    end
-  end
-  
+  let!(:todo_item)  {todo_list.todo_items.create(content: "Milk") } 
+ 
   it "is successful" do
    visit_todo_list(todo_list)
     within "#todo_item_#{todo_item.id}" do
